a62d9ebe7141f2c1fd78675d9831c1140e3eb13e
KibosJ <29429479+KibosJ@users.noreply.github.com>
2020-01-28 08:05:53 +0000

    0.2 update
    Script tidy and variable name changes

--- exportidsfromchannel.ps1
+++ exportidsfromchannel.ps1
@@ -1,18 +1,27 @@
-$user = Read-Host "What is the name of the channel? (This is the username of the channel uploader)"
+# Version 0.2
+
+$channelname = Read-Host "What is the name of the channel? (This is the username of the channel uploader)"
 $key = "" # Your YouTube API key
+$file = "C:\dump\$channelname.txt"
 
 # Get the channel ID
 $channelId = Invoke-WebRequest `
-    "https://www.googleapis.com/youtube/v3/channels?part=snippet,id%2CcontentDetails%2Cstatistics&forUsername=$user&key=$key" | `
-    ConvertFrom-Json | Select-Object -ExpandProperty items | Select-Object -ExpandProperty id
+    "https://www.googleapis.com/youtube/v3/channels?part=snippet,id%2CcontentDetails%2Cstatistics&forUsername=$channelname&key=$key" | `
+    ConvertFrom-Json | `
+    Select-Object -ExpandProperty items | `
+    Select-Object -ExpandProperty id
 
 # Get list of videos from YouTube
 $results = Invoke-WebRequest `
     "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&order=date&type=video&channelId=$channelId&key=$key" | `
     ConvertFrom-Json
-$results | Select-Object -ExpandProperty items | Select-Object -ExpandProperty id | Select-Object -ExpandProperty videoId | Add-Content C:\dump\$user.txt # Extract VideoIDs
-$items = $results | Select-Object -ExpandProperty items # Get list of Videos remaining
-$nextpage = $results | Select-Object -ExpandProperty nextPageToken # Get token for the next page
+
+    $results | Select-Object -ExpandProperty items | ` # Get all items from the search
+    Select-Object -ExpandProperty id | ` # Expand the search results
+    Select-Object -ExpandProperty videoId | ` # Extract the video IDs
+    Add-Content $file # Append video IDs to a file
+    $items = $results | Select-Object -ExpandProperty items # Get list of Videos remaining
+    $nextpage = $results | Select-Object -ExpandProperty nextPageToken # Get token for the next page
 
 # Keep moving to the next page and extracting until there is nothing left to extract
 Do {
@@ -20,7 +29,11 @@ if (-not (!$items)) {
     $results = Invoke-WebRequest `
     "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&order=date&type=video&pageToken=$nextpage&channelId=$channelId&key=$key" | `
     ConvertFrom-Json
-    $results | Select-Object -ExpandProperty items | Select-Object -ExpandProperty id | Select-Object -ExpandProperty videoId | Add-Content C:\dump\$user.txt # Extract VideoIDs
+
+    $results | Select-Object -ExpandProperty items | ` # Get all items from the search
+    Select-Object -ExpandProperty id | ` # Expand the search results
+    Select-Object -ExpandProperty videoId | `
+    Add-Content $file # Append video IDs to a file
     $items = $results | Select-Object -ExpandProperty items # Get list of Videos remaining
     $nextpage = $results | Select-Object -ExpandProperty nextPageToken # Get token for the next page
 }
@@ -29,4 +42,3 @@ else {
 }
 }
 Until (!$items)
-
